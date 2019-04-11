425d325e72a76444de365f0f0195e4225645f51a
Marco Donnici <marcod1419@gmail.com>
2019-04-11 13:32:21 -0400

    fix(shared-form-field): use className prop for Box

--- shared/components/FormField/FormField.jsx
+++ shared/components/FormField/FormField.jsx
@@ -41,7 +41,7 @@ const renderLabel = (label, hint, fieldId, tooltip) => (
     dangerouslyAddClassName={positionStyles.relative}
   >
     <label htmlFor={fieldId.identity()}>
-      <Box inline tag="span" between={2} dangerouslyAddClassName={styles.alignCenter}>
+      <Box inline tag="span" between={2} className={styles.alignCenter}>
         <Text size="medium" bold>
           {label}
         </Text>
