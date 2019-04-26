088817bcde34a3be7f949eae73d2b97f304bd939
Jordan Raffoul <me@jordanraffoul.com>
2019-04-26 14:26:37 -0400

    feat(shared-clickable): add innerRef prop to allow forwarding of refs

--- shared/components/Clickable/Clickable.jsx
+++ shared/components/Clickable/Clickable.jsx
@@ -2,36 +2,37 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import joinClassNames from '../../utils/joinClassNames'
-import safeRest from '../../utils/safeRest'
 
 import styles from './Clickable.modules.scss'
 
 /**
  * An invisible button.
  */
-const Clickable = ({ dangerouslyAddClassName, dangerouslyAddStyle, children, type, ...rest }) => (
+const Clickable = ({ children, type, className, innerRef, ...rest }) => (
   // eslint-disable-next-line react/button-has-type
   <button
     type={type}
-    {...safeRest(rest)}
-    className={joinClassNames(styles.clickable, dangerouslyAddClassName)}
-    style={dangerouslyAddStyle}
+    {...rest}
+    ref={innerRef}
+    className={joinClassNames(styles.clickable, className)}
   >
     {children}
   </button>
 )
 
 Clickable.propTypes = {
-  dangerouslyAddClassName: PropTypes.string,
-  dangerouslyAddStyle: PropTypes.object,
+  /* @ignore */
+  className: PropTypes.string,
+  /* @ignore */
+  innerRef: PropTypes.object,
   children: PropTypes.node.isRequired,
   type: PropTypes.oneOf(['submit', 'reset', 'button']),
 }
 
 Clickable.defaultProps = {
-  dangerouslyAddClassName: undefined,
-  dangerouslyAddStyle: undefined,
   type: 'button',
+  className: undefined,
+  innerRef: undefined,
 }
 
 export default Clickable
