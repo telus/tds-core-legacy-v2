3a6bcc3db6b72ae20ba4509fdd4a12bebc5e53d1
Marco Donnici <marcod1419@gmail.com>
2019-05-24 13:51:40 -0400

    feat(core-a11y-content): enable rest props

--- packages/A11yContent/A11yContent.jsx
+++ packages/A11yContent/A11yContent.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import styles from './A11yContent.modules.scss'
+import safeRest from '../../shared/utils/safeRest'
 
 /**
  * @version ./package.json
  */
 
-const A11yContent = ({ children }) => {
-  return <span className={styles.a11yContent}>{children}</span>
+const A11yContent = ({ children, ...rest }) => {
+  return (
+    <span {...safeRest(rest)} className={styles.a11yContent}>
+      {children}
+    </span>
+  )
 }
 
 A11yContent.propTypes = {
